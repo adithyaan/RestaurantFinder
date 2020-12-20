a4fea5d9c4447c148f8764d775a5f81dd484aac1
adithyaan <anirudhadi777@gmail.com>
2020-12-20 11:31:01 +0530

    distance calculation added

--- src/FindFood.java
+++ src/FindFood.java
@@ -2,18 +2,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.lang.*;
 public class FindFood {
     private static final String RESTAURANT_FILE_PATH = "/Users/mac/Downloads/Programs/JAVA/RestaturantApp/src/sample.txt";
     private static double latitude,longitude;
     private static int rating;
+    static ArrayList<Restaurant> restaurantsList;
 
-    double dist(double x1, double y1, double x2, double y2) {
-        return new Double(1);
+    private static double dist(double x1,double y1, double x2, double y2){
+
+        double d = 0;
+        double nx1=Math.toRadians(x1);
+        double nx2=Math.toRadians(x2);
+        double ny1=Math.toRadians(y1);
+        double ny2=Math.toRadians(y2);
+
+        double sx1=Math.cos(x1);
+        double sx2=Math.sin(x2);
+        double cx1=Math.cos(x1);
+        double cx2=Math.cos(x2);
+        double yd=ny1-ny2;
+        double cyd=Math.cos(yd);
+
+        d = 1.1516 * 60 * Math.acos( (sx1*sx2) + (cx1 * cx2 *cyd));
+        return d;
     }
 
-    int findNext(double x, double y, double[] lat, double[] lon){
-        return 0;
+    private static double findNext(double x, double y, double lat, double lon){
+        double distance = dist(x,y,lat,lon);
+
+        return distance;
     }
 
     void markFound(int f, double[] lat, double[] lon){
@@ -21,11 +39,22 @@ public class FindFood {
     }
 
     public static void main(String[] args) {
-        ArrayList<Restaurant> restaurantsList = readRestaurantData();
+        restaurantsList = readRestaurantData();
+        ArrayList<Restaurant> outputList = new ArrayList<>();
         getInput();
         restaurantsList.forEach((restaurant -> {
-            System.out.println(restaurant.getName());
+            if(restaurant.getRating() > rating) {
+                double distance = findNext(latitude, longitude, restaurant.getLatitude(), restaurant.getLongitude());
+                restaurant.setDistance(distance);
+                outputList.add(restaurant);
+            }
         }));
+
+        for (Restaurant re:outputList){
+            System.out.println(re.getName()+"has a rating of  "+re.getRating() );
+            System.out.println("Distance "+ re.getDistance()+" miles");
+        }
+
 
     }
 
@@ -38,10 +67,9 @@ public class FindFood {
                 String data = myReader.nextLine();
                 String arr[] = data.split(" ");
                 if(arr.length>1) {
-                    Restaurant restaurant = new Restaurant(arr[0], Double.parseDouble(arr[1]), Double.parseDouble(arr[2]), Integer.parseInt(arr[3]));
+                    Restaurant restaurant = new Restaurant(arr[0], Double.parseDouble(arr[1]), Double.parseDouble(arr[2]), Integer.parseInt(arr[3]),0);
                     restaurantsList.add(restaurant);
                 }
-
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -59,8 +87,6 @@ public class FindFood {
         longitude = scanner.nextDouble();
         System.out.println("Enter minimum rating");
         rating = scanner.nextInt();
-
-
     }
 
 }
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -2,12 +2,14 @@ public class Restaurant {
     private String name;
     private double latitude,longitude;
     private int rating;
+    private double distance;
 
-    public Restaurant(String name, double latitude, double longitude, int rating) {
+    public Restaurant(String name, double latitude, double longitude, int rating,double distance) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
         this.rating = rating;
+        this.distance = distance;
     }
 
     public String getName() {
@@ -40,5 +42,13 @@ public class Restaurant {
 
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 }
